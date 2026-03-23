8eb252ea3ef5cb7c5ec432ef3c02737b7c133c1a
ARJUN <pachangedinanath@gmail.com>
2026-03-24 04:26:35 +0530

    Update start.py

--- Oneforall/plugins/bot/start.py
+++ Oneforall/plugins/bot/start.py
@@ -36,6 +36,10 @@ FORCE_CHANNEL_2 = config.FORCE_CHANNEL_2
 
 NEXT_IMG = [
     "https://graph.org/file/d22cec7c75e26f36edff7-0ce8cae0037d4aa0aa.jpg",
+
+"https://graph.org/file/37121598cf32913ff783c-e0526226ace3708a89.jpg",
+
+"https://graph.org/file/82379bbebbdd4ec7aba3e-d3a42778d30fbfa9aa.jpg",
     "https://graph.org/file/c53dfca85e9e0b5bc9cd1-afa1339cd5d4e6522c.jpg",
     "https://graph.org/file/3a907478d7a13ff1e8ecb-606e29f797b109ab82.jpg",
     "https://graph.org/file/35f6ffeeac9c330200742-eecc5ab1977d58e06b.jpg",

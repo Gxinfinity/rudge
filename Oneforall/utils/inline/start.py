71ed0fd01b1bdd735c975ead28e1538c593a6f61
Gxinfinity <190551265+Gxinfinity@users.noreply.github.com>
2026-04-03 21:52:45 +0530

    Update start.py

--- Oneforall/utils/inline/start.py
+++ Oneforall/utils/inline/start.py
@@ -1,16 +1,31 @@
 from pyrogram.types import InlineKeyboardButton
 from pyrogram.enums import ButtonStyle
 import config
-from Oneforall import app
+from EsproMusic import app
+
+
+# safe button function (NO CRASH)
+def btn(text, **kwargs):
+    try:
+        return InlineKeyboardButton(
+            text=text,
+            style=ButtonStyle.PRIMARY,
+            icon_custom_emoji_id=6312260233171312151,
+            **kwargs
+        )
+    except TypeError:
+        return InlineKeyboardButton(
+            text=text,
+            style=ButtonStyle.PRIMARY,
+            **kwargs
+        )
 
 
 def start_panel(_):
     buttons = [
         [
-            InlineKeyboardButton(
-                text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
-            ),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+            btn(_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"),
+            btn(_["S_B_2"], url=config.SUPPORT_CHAT),
         ],
     ]
     return buttons
@@ -19,21 +34,20 @@ def start_panel(_):
 def private_panel(_):
     buttons = [
         [
-            InlineKeyboardButton(
-                text=_["S_B_3"],
-                url=f"https://t.me/{app.username}?startgroup=true",style=ButtonStyle.PRIMARY
-            )
+            btn(_["S_B_3"], url=f"https://t.me/{app.username}?startgroup=true")
         ],
         [
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT,style=ButtonStyle.PRIMARY),
-            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL,style=ButtonStyle.PRIMARY),
-        ],
-        [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper",style=ButtonStyle.PRIMARY)],
-        [
-            InlineKeyboardButton(text=_["S_B_5"], url="https://t.me/docker_git_bit"),
+            btn(_["S_B_2"], url=config.SUPPORT_CHAT),
+            btn(_["S_B_6"], url=config.SUPPORT_CHANNEL),
         ],
         [
-            InlineKeyboardButton(text="✦ ᴡєʙ ɢᴧϻєꜱ 🎮✨", url="https://telegram-game-hub.vercel.app",style=ButtonStyle.PRIMARY)
+            btn(_["S_B_4"], callback_data="settings_back_helper")
+        ],
+        [
+            btn(_["S_B_5"], url="https://t.me/docker_git_bit")
+        ],
+        [
+            btn("✦ ᴡєʙ ɢᴧϻєꜱ 🎮✨", url="https://telegram-game-hub.vercel.app")
         ],
     ]
     return buttons
